d24377a3e762a3b6f5cb136ab39be3e2c2bbb506
jamie <northwing@naver.com>
2025-07-10 18:05:58 +0900

    스프링 컨피그 호환을 위한 다운그레이드

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.4.7"
+    id("org.springframework.boot") version "3.3.1"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.bookshop"
 version = "0.0.1-SNAPSHOT"
 
-val springCloudVersion = "2023.0.1"
+val springCloudVersion = "2023.0.1" //사용할 클라우드 버전
 
 java {
     toolchain {
@@ -28,7 +28,7 @@ dependencies {
 }
 
 dependencyManagement {
-    imports {
+    imports {//스프링 클라우드 의존성 관리를 위한 DOM
         mavenBom("org.springframework.cloud:spring-cloud-dependencies:${springCloudVersion}")
     }
 }
