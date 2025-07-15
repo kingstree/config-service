84ba548c451692fc069aa49f5992e6237e52f436
jamie <northwing@naver.com>
2025-07-15 23:44:30 +0900

    7.6 매니페스트 유효성 검사 자동화 . 6장, 7장 내용 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.3.1"
+    id("org.springframework.boot") version "3.5.3"          // 안정판
     id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -29,10 +29,25 @@ dependencies {
 
 dependencyManagement {
     imports {//스프링 클라우드 의존성 관리를 위한 DOM
+        mavenBom("org.springframework.boot:spring-boot-dependencies:3.3.1")
         mavenBom("org.springframework.cloud:spring-cloud-dependencies:${springCloudVersion}")
     }
 }
+tasks.bootBuildImage {
+    builder.set("paketobuildpacks/builder-jammy-java-tiny:0.0.46")
+    imagePlatform.set("linux/arm64")
+    imageName.set(project.name)
+    imageName.set("ghcr.io/kingstree/${project.name}:latest")   // ★ 레지스트리·계정 포함
+    environment.put("BP_JVM_VERSION", "17")
 
+    docker {
+        publishRegistry {
+            username = project.findProperty("registryUsername") as String?
+            password = project.findProperty("registryToken") as String?
+            url = project.findProperty("registryUrl") as String?
+        }
+    }
+}
 tasks.withType<Test> {
     useJUnitPlatform()
 }
